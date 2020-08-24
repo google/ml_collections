10711aa50288c6324bfc8aeb01322e57bfa9ad29
Mohit Reddy <mohitreddy@google.com>
2020-08-24 01:09:16 -0700

    Do not perform full update in the pythonenv when test.sh is executed.
    PiperOrigin-RevId: 328095869
    Change-Id: Idbdce794ada90e1836de75569f04d228a17c00ae

--- test.sh
+++ test.sh
@@ -38,7 +38,6 @@ curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 
 sudo apt update && sudo apt install bazel
-sudo apt update && sudo apt full-upgrade
 
 # Run bazel test
 bazel test ml_collections/...
